186b9905fd90e296791db20db7b2cf5c4e47a1a7
MattioCh <matthewchuang19@gmail.com>
2021-10-28 20:13:16 +0800

    fix commit and merge

---
+++ src/Posts/pages.js
@@ -0,0 +1,95 @@
+import React, { Component , useState   }  from 'react';
+
+
+
+export function Page1() {
+    return (
+        <div>
+            <h1>Matthew Chuang</h1>
+            <p><em>Born and raised in Taiwan</em></p>
+    </div>
+    )
+  };
+export function Page2() {
+    return (
+        <div>
+            <p><em>A Mathematics and Economics student</em></p>
+            <p><em>And a self-taught programmer</em></p>
+
+
+    </div>
+    )
+  };
+export function Page3() {
+    return (
+        <div>
+        <h2>I like to code a lot 💻⌨️</h2>
+    </div>
+    )
+  };
+export function Page4() {
+    return (
+        <div>
+            <h2>My past projects</h2>
+            <ul>
+                <li><b>Chinese Machine Reading Comprehension:</b><br></br> Question Answering AI</li>
+                <br></br>
+                <li><b>Dataset Generation:</b><br></br>Question-Answer Pair Generation through reading an article</li>
+                <br></br>
+                <li><b>Information Retrieval System:</b><br></br> find relevent documents from 8M of them in 0.4 seconds</li>
+                <br></br>
+                <li><b>Stock Backtesting System:</b><br></br>setup the testing environment and backtested several statistical and ML-based trading strategies</li>
+                <br></br>
+                <li><b>Competitive Programming Codeforce:</b><br></br>Best ranking -> top 20% out of 40000 (still improving )</li>
+
+            </ul>
+    </div>
+    )
+};
+
+export function Page5() {
+    return (
+        <div>
+            <h3>I learn fast<br></br>I can adapt to a new framework quickly</h3>
+        </div>
+    )
+};
+
+export function Page6() {
+    return (
+        <div>
+            <h2>I am also a research assistant in NLP domain</h2>
+            <h4>Topic: debiasing of large language model</h4>
+        </div>
+    )
+};
+
+
+export function Page7() {
+    return (
+        <div>
+            <h2>I volunteer!</h2>
+            <h3>I am a project manager in a charity now.</h3>
+            <h3>I organised, hosted monthly meeetup meeting events to<br></br> discuss social issues. A bit like Ted Talk but smaller</h3>
+        </div>
+    )
+};
+
+export function Page8() {
+    return (
+        <div>
+            <h3>I like the following concept<br></br>Hope some of them resonates with you</h3>
+            <h4>#rationalist<br></br>#lesswrong<br></br>#effective_altruism<br></br>#regression<br></br>#startup<br></br>#recommender_system_problem<br></br>#machine_learning</h4>
+        </div>
+    )
+};
+
+export function Page9() {
+    return (
+        <div>
+            <h3>This is linkedin and email</h3>
+            <a href="https://www.linkedin.com/in/matthew-chuang/">Linkedin</a>
+            <a href="matthewchuang19@gmail.com">Email: matthewchuang19@gmail.com</a>
+        </div>
+    )
+};
